8077a317130dc00691383b8e8c0582266dca67d6
kirill1852892 <kirillstoyanov10@gmail.com>
2025-01-11 14:28:50 +0200

    products

---
+++ script.js
@@ -0,0 +1,15 @@
+let product = [
+    {
+        "title":"холодильник",
+        "description":"Side-by-side холодильник BOSCH KAI93VI304",
+        "price":"85799",
+        "image":"https://content1.rozetka.com.ua/goods/images/big/181827426.jpg",
+    },
+]
+function getCookieValue(CookieName) {
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+        consl cookie = cookies[i].trim();
+    }
+    
